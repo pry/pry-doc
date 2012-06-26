c69b7ec78e3d1ce01f98a6c1d41846f8662d8f1f
Reginald Tan <redge.tan@gmail.com>
2012-06-26 10:20:15 -0400

    Updated test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,8 @@ class C
   def message; end
 end
 
-unless File.exists?("#{direc}/gem_with_cext/ext/Makefile")
-  puts
-  puts "Building Sample Gem with C Extensions for testing.."
-  system("cd #{direc}/gem_with_cext/ext/ && ruby extconf.rb && make")
-  puts
-end
+puts
+puts "Building Sample Gem with C Extensions for testing.."
+system("cd #{direc}/gem_with_cext/gems/ext/ && ruby extconf.rb && make")
+puts
 
