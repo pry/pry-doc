57e3e8e4575707142eba5acb7bff6b02d67d2bff
John Mair <jrmair@gmail.com>
2018-02-06 14:18:52 +0100

    Subclass show-source instead of monkey-patching pry

--- lib/pry-doc/pry_ext/c_source_lookup.rb
+++ lib/pry-doc/pry_ext/c_source_lookup.rb
@@ -34,15 +34,50 @@ class CFile
   end
 end
 
-class ShowCSource < Pry::ClassCommand
-  match 'show-c'
+class ShowSourceWithCInternals < Pry::Command::ShowSource
+  def options(opt)
+    super(opt)
+    opt.on :c, "c-source", "Show source of a C symbol in MRI"
+  end
+
+  def extract_c_source
+    if opts.present?(:all)
+      result = ShowCSource.new.show_all_definitions(obj_name)
+    else
+      result = ShowCSource.new.show_first_definition(obj_name)
+    end
+    if result
+      _pry_.pager.page result
+    else
+      raise CommandError, no_definition_message
+    end
+  end
+
+  def process
+    if opts.present?(:c)
+      extract_c_source
+      return
+    else
+      super
+    end
+  rescue Pry::CommandError
+    extract_c_source
+  end
+end
+
+class ShowCSource < Pry::Command::ShowSource
+  match 'show-source'
   group 'Introspection'
-  description 'show c source'
+  description 'show source'
 
   class << self
     attr_accessor :file_cache
   end
   @file_cache = {}
+
+  def initialize(*)
+    super
+  end
 
   def balanced?(str)
     tokens = CodeRay.scan(str, :c).tokens.each_slice(2).to_a
@@ -81,6 +116,11 @@ class ShowCSource < Pry::ClassCommand
       break code unless source_file[info.line + offset - 1].strip.end_with?('\\')
       offset += 1
     end
+  end
+
+  def extract_typedef_oneliner(info)
+    source_file = source_from_file(info.file)
+    return source_file[info.line]
   end
 
   def extract_function(info)
@@ -127,59 +167,77 @@ class ShowCSource < Pry::ClassCommand
   end
 
   def options(opt)
-    opt.on :l, "line-numbers", "Show line numbers"
-    opt.on :b, "base-one", "Show line numbers but start numbering at 1"
-    opt.on :a, :all,  "Show all definitions of the C function"
+    super(opt)
+    #      opt.on :c, "c-source", "Show source of a C symbol in MRI" if defined?(ShowCSource)
+    # opt.on :l, "line-numbers", "Show line numbers"
+    # opt.on :b, "base-one", "Show line numbers but start numbering at 1"
+    # opt.on :a, :all,  "Show all definitions of the C function"
   end
 
-  def process(x)
-    infos = self.class.symbol_map[x]
-    if infos.nil?
-      output.puts "Error: Couldn't locate a definition for #{x}"
-      return
-    end
-
+  def process
+    super
+  rescue Pry::CommandError
     if opts.present?(:all)
-      result = show_all_definitions(infos)
+      result = ShowCSource.new.show_all_definitions(obj_name)
     else
-      result = show_first_definition(infos.first, infos.count)
+      result = ShowCSource.new.show_first_definition(obj_name)
     end
-
-    _pry_.pager.page result
+    if result
+      _pry_.pager.page result
+    else
+      raise Pry::CommandError, no_definition_message
+    end
   end
+
+  #   infos = self.class.symbol_map[x]
+  #   if infos.nil?
+  #     output.puts "Error: Couldn't locate a definition for #{x}"
+  #     return
+  #   end
+
+  #   if opts.present?(:all)
+  #     result = show_all_definitions(infos)
+  #   else
+  #     result = show_first_definition(infos.first, infos.count)
+  #   end
+
+  #   _pry_.pager.page result
+  # end
 
   def show_all_definitions(x)
     infos = self.class.symbol_map[x]
     return unless infos
 
     result = ""
-    infos.each do |info|
-      result << show_first_definition(x, false) << "\n"
+    infos.count.times do |index|
+      result << show_first_definition(x, index) << "\n"
     end
     result
   end
 
-  def show_first_definition(x, show_count=true)
+  def show_first_definition(x, index=nil)
     infos = self.class.symbol_map[x]
     return unless infos
 
     count = infos.count
-    info = infos.first
+    info = infos[index || 0]
     code = if info.original_symbol.start_with?("#define")
              extract_macro(info)
            elsif info.original_symbol =~ /\s*struct\s*/ || info.original_symbol.start_with?("enum")
              extract_struct(info)
            elsif info.original_symbol.start_with?("}")
              extract_typedef_struct(info)
+           elsif info.original_symbol =~/^typedef.*;$/
+             extract_typedef_oneliner(info)
            else
              extract_function(info)
            end
 
     h = "\n#{text.bold('From: ')}#{info.file} @ line #{info.line}:\n"
-    h << "#{text.bold('Number of implementations:')} #{count}\n" if show_count
+    h << "#{text.bold('Number of implementations:')} #{count}\n" unless index
     h << "#{text.bold('Number of lines: ')} #{code.lines.count}\n\n"
     h << Pry::Code.new(code, 1, :c).
-               with_line_numbers(false).highlighted
+           with_line_numbers(false).highlighted
   end
 
   def start_line_for(info)
@@ -229,6 +287,6 @@ class ShowCSource < Pry::ClassCommand
       h.merge!(v.symbols) { |k, old_val, new_val| old_val + new_val }
     end
   end
-end
 
-Pry::Commands.add_command(ShowCSource)
+  Pry::Commands.add_command(ShowSourceWithCInternals)
+end
